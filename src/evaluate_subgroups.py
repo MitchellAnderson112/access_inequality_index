fbc03d0043e1c35ea658916cb3ba13cb963418d7
Tom Logan <tom.logan@canterbury.ac.nz>
2020-04-12 16:10:00 +1200

    making it easier to do subgroup analysis

--- src/evaluate_subgroups.py
+++ src/evaluate_subgroups.py
@@ -9,208 +9,190 @@ For given beta and states, will take data from SQL with population and distance 
 '''
 
 # User defined variables
-kapa = -0.170477678 #This should be equal to kapa used for city comparisons
-beta = -0.5
-epsilon = 0.5
-races = ['H7X001', 'H7X002', 'H7X003', 'H7X004', 'H7X005', 'H7Y003']
-race_labels = ['all', 'white', 'black', 'american_indian', 'asian', 'hispanic'] #Black and african american, indiand and native alaskan, hispanic and latino
-state = input('State: ')
+beta = -1.0
+states = ['md','fl', 'co', 'mi', 'la', 'ga', 'or', 'il', 'wa', 'tx']
+cities = {'md':'baltimore','fl':'miami','co':'denver','mi':'detroit','la':'new orleans','ga':'atlanta','or':'portland','il':'chicago','wa':'seattle','tx':'houston'}
 
-#imports
+# Imports
 import utils
 from config import *
-db, context = cfg_init(state)
-cursor = db['con'].cursor()
-
-file_name = '{}_race_results'.format(context['city'])
+import inequality_function
+import matplotlib
+from tqdm import tqdm
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+import matplotlib.style as style
+style.use('fivethirtyeight')
+w = 5
+h = w/1.618
 
 def main():
-    df, city = get_df()
-    results = pd.DataFrame(np.nan, index=np.arange(6), columns=['State', 'City', 'Race', 'Race_Label', 'Kapa', 'Beta', 'Epsilon', 'Kolm-Pollak EDE', 'Atkinson EDE', 'Atkinson Adjusted EDE', 'Kolm-Pollak Index', 'Atkinson Index', 'Atkinson Adjusted Index', 'Gini Index', 'Distribution Mean', 'Distribution Max', 'Distribution Standard Deviation', 'Distribution Coefficient of Variation'])
-    # adds race
-    results.Race_Label = race_labels
-    results.Race = races
-    #sets index to race
-    results = results.set_index('Race')
-    for race in races:
-        # adds the city name
-        results.City = city
-        # adds aversion params
-        results.loc[race, 'Kapa'], results.loc[race, 'Beta'], results.loc[race, 'Epsilon'] = kapa, beta, epsilon
-        # adds all Kolm-Pollak metrics
-        results.loc[race, 'Kolm-Pollak Index'], results.loc[race, 'Kolm-Pollak EDE'] = get_kolm(df, kapa, race)
-        # adds all normal (with inverted x) atkinson metrics
-        results.loc[race, 'Atkinson Index'], results.loc[race, 'Atkinson EDE'] = get_at(df, race)
-        # adds all adjusted atkinson metrics
-        results.loc[race, 'Atkinson Adjusted Index'], results.loc[race, 'Atkinson Adjusted EDE'] = get_at_adj(df, race)
-        # adds gini
-        results.loc[race, 'Gini Index'] = get_gini(df, race)
-        # adds all summary stats from the distribution
-        results.loc[race, 'Distribution Mean'], results.loc[race, 'Distribution Max'], results.loc[race, 'Distribution Standard Deviation'], results.loc[race, 'Distribution Coefficient of Variation'] = get_stats(df, race)
+    '''Calculates the EDE for different subgroups'''
+    city, data = get_data()
+    # kappa is based on the distances from ALL states and the beta provided
+    kappa = determine_kappa(data, beta, quantity='distance')
+    # subgroups to investigate
+    subgroups = ['H7X001','H7X002','H7X003','H7X004','H7X005','H7Y003',
+                 'poverty','not_poverty','vehicle_access','no_vehicle_access']
+    # calculate two subgroups
+    for state in states:
+        data['{}_data'.format(state)] = estimate_poverty(data['{}_data'.format(state)])
+        data['{}_data'.format(state)] = estimate_vehicle(data['{}_data'.format(state)])
+    # initialize dataframe
+    results = []
+    # evaluate equality
+    for state in tqdm(states):
+        # loop through the subgroups
+        for group in subgroups:
+            # Gets the df for specific state
+            df = data['{}_data'.format(state)].copy()
+            # drop data that has 0 weight
+            df = df.iloc[np.array(df[group]) > 0].copy()
+            # calculate the ede
+            ede = inequality_function.kolm_pollak_ede(list(df.distance), kappa = kappa, weight = list(df[group]))
+            # new result
+            result_i = [cities[state], group, ede]
+            results.append(result_i)
+    # make list of lists a dataframe
+    results = pd.DataFrame(results, columns = ['city','group','ede'])
+    # save result
+    results.to_csv('/homedirs/man112/access_inequality_index/data/results/ede_subgroups_{}.csv'.format(beta))
+    # plots
+    plot_race(results)
+    plot_poverty(results)
+    plot_vehicle(results)
 
-    plot_gini(df)
-    plot_hist(df)
-    plot_cdf(df)
-    results.to_csv(r'/homedirs/man112/access_inequality_index/data/results/{}.csv'.format(file_name))
+def get_data():
+    '''fills a dictionary of dataframes for each state'''
+    data = {} # init empty dictionary
+    city = [] #init empty list of cities to return for the results
+    for state in states:
+        db, context = cfg_init(state)
+        cursor = db['con'].cursor()
+        sql = 'SELECT * FROM distxdem'
+        data["{}_data".format(state)] = pd.read_sql(sql, db["con"])
+        db['con'].close()
+        city.append(context['city'])
+        df = data['{}_data'.format(state)]
+        df = df.loc[df['distance'] !=0] # removes all rows with 0 distance
+        df = df.loc[df['H7X001'] !=0] # removes all rows with 0 population
+        df.distance = df.distance/1000 # converts from meters to Kms
+        data['{}_data'.format(state)] = df # replaces the dataframe in the dictionary
+    return(city, data)
 
-def get_df():
-    #Makes a dictionary of nearest dist and demo
-    sql = 'SELECT * FROM distxdem'
-    df = pd.read_sql(sql, db["con"])
-    db['con'].close()
-    city = context['city']
-
-    #Sorts the dfs for each state and gives population columns
-    kapa_data = []
-    df = df.loc[df['distance'] !=0]
-    df = df.loc[df['H7X001'] !=0]
-    df = df.dropna()
-    return(df, city)
-
-def get_gini(df, race):
-    df = df.loc[df[race]!=0]
-    dist_tot = df['distance'].sum()
-    pop_tot = df[race].sum()
-    df = df.sort_values(by='distance')
-    df['pop_perc'] = df[race].cumsum()/pop_tot*100
-    df['dist_perc'] = df['distance'].cumsum()/dist_tot*100
-    area_tot = simps(np.arange(0,101,1), dx=1)
-    area_real = simps(df['dist_perc'], df['pop_perc'])
-    area_diff = area_tot - area_real
-    gini = area_diff/area_tot
-    gini = round(gini, 3)
-    #gini = 1
-    return(gini)
-
-def get_at_adj(df, race):
-    df = df.loc[df[race]!=0]
-    at_sum = 0
-    count = 0
-    N = df[race].sum()
-    x_mean = np.average(df['distance'], weights = df[race])/1000
-    for i in df['distance']/1000:
-        at_sum += (i**(1-beta))*df[race].iloc[count]
-        count += 1
-    at_ede = (at_sum/N)**(1/(1-beta))
-    at_ind = (at_ede/x_mean)-1
-    return(at_ind, at_ede)
-
-def get_at(df, race):
-    df = df.loc[df[race]!=0]
-    at_sum = 0
-    N = df[race].sum()
-    x_mean = np.average(df['distance'], weights = df[race])/1000
-    count = 0
-    for i in df['distance']/1000:
-        i = 1/i
-        at_sum += (i**(1-epsilon))*df[race].iloc[count]
-        count += 1
-    at_ede = (at_sum/N)**(1/(1-epsilon))
-    at_ind = 1 - (at_ede/x_mean)
-    return(at_ind, at_ede)
-
-def get_kolm(df, kapa, race):
-    df = df.loc[df[race]!=0]
-    N = df[race].sum()
-    x_mean = np.average(df['distance'], weights = df[race])/1000
-    sum_ede = 0
-    sum_ii = 0
-    count = 0
-    for x_n in df['distance']/1000:
-        sum_ede += np.exp(-kapa*x_n)*df[race].iloc[count]
-        sum_ii += np.exp(-kapa*(x_n-x_mean))*df[race].iloc[count]
-        count += 1
-    kp_ede = (-1/kapa)*np.log(sum_ede/N)
-    kp_ind = -(1/kapa)*np.log(sum_ii/N)
-    return(kp_ind, kp_ede)
-
-def get_stats(df, race):
-    df = df.loc[df[race]!=0]
-    pop_total = df[race].sum()
-    df = df.sort_values(by='distance')
-    hist_data = []
-    count =0
-    for i in df['distance']/1000:
-        for pop in range(int(df[race].iloc[count])):
-            hist_data.append(i)
-        count += 1
-    mean = np.mean(hist_data)
-    max = np.max(hist_data)
-    std = np.std(hist_data)
-    cov = std/mean
-    return(mean, max, std, cov)
-
-def plot_gini(df):
-    for race in races:
-        df = df.loc[df[race]!=0]
-        pop_tot = df[race].sum()
-        dist_tot = df.distance.sum()
-        df = df.sort_values(by='distance')
-        df['pop_perc'] = df[race].cumsum()/pop_tot*100
-        df['dist_perc'] = df.distance.cumsum()/dist_tot*100
-        plt.plot(df.pop_perc, df.dist_perc, label=race)
-    plt.plot(np.arange(0,101,1), np.arange(0, 101, 1), '--', color='black', lw=0.5, label = 'Perfect Equality Line')
-    plt.xlabel('% Residents')
-    # xlabel
-    plt.ylabel('% Distance')
-    plt.xlim([0,None])
-    plt.ylim([0,None])
-    plt.title('Gini Curve'.format(loc='center'))
-    plt.legend(loc='best')
-    #Save figrue
-    fig_out = '/homedirs/man112/access_inequality_index/data/results/{}_GINI_race_test.pdf'.format(context['city'])
-    if os.path.isfile(fig_out):
-        os.remove(fig_out)
-    plt.savefig(fig_out, dpi=500, format='pdf', transparent=False)#, bbox_inches='tight')
-    plt.clf()
-
-def plot_hist(df):
-
-    fig, axes = plt.subplots(ncols=2,nrows=3, sharex=True, sharey=True, gridspec_kw={'hspace':0.5})
-    for race, ax in zip(races, axes.flat):
-        df = df.loc[df[race]!=0]
-        pop_tot = df[race].sum()
-        df = df.sort_values(by='distance')
-        hist_data = []
+def determine_kappa(data, beta, quantity):
+    '''takes dictionary of dataframes and beta, minimised the sum of squares for all data to return kappa'''
+    # prepare the data
+    kappa_data = [] # init empty list for each distance
+    for state in states:
+        df = data['{}_data'.format(state)]
         count = 0
-        for i in tqdm((df.distance)/1000):
-            for pop in range(df[race].iloc[count]):
-                hist_data.append(i)
-        sns.distplot(hist_data, hist = True, kde = True, bins = int(100), label = race, ax=ax, color=random.choice(['red','blue','green','yellow','orange','purple', 'pink']), kde_kws={'color':'black'})
-        ax.title.set_text(race)
-    plt.xlim([0,20])
-    plt.ylim([0,None])
+        for i in df[quantity]: #takes each distance
+            if not np.isnan(i):
+                for pop in range(int(df['H7X001'].iloc[count])): #adds the distance to the list as many times as there are people in the block
+                    kappa_data.append(i)
+                count += 1
+    # calculate the kappa
+    kappa = inequality_function.calc_kappa(kappa_data, beta)
+    return(kappa)
 
-    fig_out = '/homedirs/man112/access_inequality_index/data/results/{}_HIST_race_test.pdf'.format(context['city'])
-    if os.path.isfile(fig_out):
-        os.remove(fig_out)
-    plt.savefig(fig_out, format='pdf')#, bbox_inches='tight')
+def estimate_poverty(df):
+    # JOCE001 - block group population; JOCE002 - poverty ratio < 0.5; JOCE003 - poverty ratio [0.5,1)
+    percentage_poverty = (df['JOCE002'] + df['JOCE003'])/df['JOCE001']
+    df['poverty'] = percentage_poverty * df['H7X001']
+    df['not_poverty'] = (1-percentage_poverty) * df['H7X001']
+    df['poverty'] = df['poverty'].fillna(0)
+    df['not_poverty'] = df['not_poverty'].fillna(0)
+    df['poverty'] = df['poverty'].astype(int)
+    df['not_poverty'] = df['not_poverty'].astype(int)
+    return df
+
+def estimate_vehicle(df):
+    # JSNE001 - total housing units; JSNE003 - owner occupied houses with no vehicles, JSNE010 - renter occupied, no vehicles
+    percentage = (df['JSNE003'] + df['JSNE010'])/df['JSNE001']
+    df['no_vehicle_access'] = percentage * df['H7X001']
+    df['vehicle_access'] = (1-percentage) * df['H7X001']
+    df['no_vehicle_access'] = df['no_vehicle_access'].fillna(0)
+    df['vehicle_access'] = df['vehicle_access'].fillna(0)
+    df['no_vehicle_access'] = df['no_vehicle_access'].astype(int)
+    df['vehicle_access'] = df['vehicle_access'].astype(int)
+    return df
+
+###
+# Plots
+###
+def plot_race(results):
+    '''plots the ede for different racial groups'''
+    results = results.pivot(index='city', columns='group', values='ede')
+    # sort the data by the KP EDE
+    results = results.sort_values(by='H7X001')
+    # plot on a line graph
+    ax = plt.axes()
+    plt.locator_params(axis='y', nbins=4)
+    results.plot(y=["H7X001", "H7X002", "H7X003","H7Y003"],ax=ax)
+    plt.ylim([0, None])
+    plt.xticks(range(10),results.index.values)
+    plt.xticks(rotation=90)
+    plt.axhline(y = 0, color = 'black', linewidth = 1.3, alpha = .7)
+    fig_out = '/homedirs/man112/access_inequality_index/fig/ede_subgroup_race_{}.pdf'.format(beta)
+    plt.savefig(fig_out, dpi=500, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
+    plt.show()
     plt.clf()
 
-def plot_cdf(df):
-    for race in races:
-        df = df.loc[df[race]!=0]
-        pop_tot = df[race].sum()
-        df = df.sort_values(by='distance')
-        df['pop_perc'] = df[race].cumsum()/pop_tot*100
-        plt.plot(df.distance/1000, df.pop_perc, label = race)
-    plt.plot(np.arange(0,100,100/10000), 10000*[100], '--')
-    # ylabel
-    plt.ylabel('% Residents')
-    # xlabel
-    plt.xlabel('Distance to the nearest supermakrt (km)'.format())
-    plt.xlim([0,15])
-    plt.ylim([0,None])
-    #making the title todays date and the time_stamp
-    plt.title('CDF: Distance to the nearest supermarket'.format(), loc='center')
-    plt.legend(loc='best')
-
-    # savefig
-    fig_out = '/homedirs/man112/access_inequality_index/data/results/{}_CDF_race_test.pdf'.format(context['city'])
-    if os.path.isfile(fig_out):
-        os.remove(fig_out)
-    plt.savefig(fig_out, dpi=500, format='pdf', transparent=False)#, bbox_inches='tight')
+def plot_vehicle(results):
+    '''plots the ede for whether people have access to vehicles'''
+    results = results.pivot(index='city', columns='group', values='ede')
+    # sort the data by the KP EDE
+    results = results.sort_values(by='H7X001')
+    # plot on a line graph
+    ax = plt.axes()
+    plt.locator_params(axis='y', nbins=4)
+    results.plot(y=["H7X001", "vehicle_access", "no_vehicle_access"],ax=ax)
+    plt.ylim([0, None])
+    plt.xticks(range(10),results.index.values)
+    plt.xticks(rotation=90)
+    plt.axhline(y = 0, color = 'black', linewidth = 1.3, alpha = .7)
+    fig_out = '/homedirs/man112/access_inequality_index/fig/ede_subgroup_vehicle_{}.pdf'.format(beta)
+    plt.savefig(fig_out, dpi=500, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
+    plt.show()
     plt.clf()
+
+def plot_poverty(results):
+    '''plots the ede for poverty status'''
+    results = results.pivot(index='city', columns='group', values='ede')
+    # sort the data by the KP EDE
+    results = results.sort_values(by='H7X001')
+    # plot on a line graph
+    ax = plt.axes()
+    plt.locator_params(axis='y', nbins=4)
+    results.plot(y=["H7X001", "not_poverty", "poverty"],ax=ax)
+    plt.ylim([0, None])
+    plt.xticks(range(10),results.index.values)
+    plt.xticks(rotation=90)
+    plt.axhline(y = 0, color = 'black', linewidth = 1.3, alpha = .7)
+    fig_out = '/homedirs/man112/access_inequality_index/fig/ede_subgroup_poverty_{}.pdf'.format(beta)
+    plt.savefig(fig_out, dpi=500, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
+    plt.show()
+    plt.clf()
+
+def plot_ie(results):
+    # this won't work unless you calculate the KP IE (currently the code is just calculating EDE)
+    results = results.sort_values(by='KP_EDE_H7X001')
+    # plot the indices on another line graph
+    ax = plt.axes()
+    plt.locator_params(axis='y', nbins=3)
+    results.plot(x="City", y=["KP_IE_H7X001", "KP_IE_H7X002", "KP_IE_H7X003", "KP_IE_H7Y003"],ax=ax)
+    plt.ylim([0, 1])
+    plt.xticks(range(10),results.City)
+    plt.xticks(rotation=90)
+    plt.axhline(y = 0, color = 'black', linewidth = 1.3, alpha = .7)
+    fig_out = '/homedirs/man112/access_inequality_index/fig/index_race_compare.pdf'.format()
+    # plt.show()
+    plt.savefig(fig_out, dpi=500, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
+
+
+
+
 
 if __name__ == '__main__':
     main()
