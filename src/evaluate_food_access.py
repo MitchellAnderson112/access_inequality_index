f75b723bd1c3d9f83afc1d5b3296c2bb75c6f0f2
Tom Logan <tom.logan@canterbury.ac.nz>
2020-04-12 15:04:26 +1200

    plots for sensitivity

--- src/evaluate_food_access.py
+++ src/evaluate_food_access.py
@@ -9,8 +9,7 @@ For given beta and states, will take data from SQL with population and distance 
 '''
 
 # User defined variables
-beta = -0.5
-epsilon = 0.5
+beta = -1.0
 states = ['md','fl', 'co', 'mi', 'la', 'ga', 'or', 'il', 'wa', 'tx']
 cities = {'md':'baltimore','fl':'miami','co':'denver','mi':'detroit','la':'new orleans','ga':'atlanta','or':'portland','il':'chicago','wa':'seattle','tx':'houston'}
 file_name = 'food_des_{}'.format(beta)
@@ -32,9 +31,10 @@ def main():
     '''Creates dataframe and adds data for each city before plotting and exporting CSV'''
     city, data = get_data()
     # kappa is based on the distances from ALL states and the beta provided
-    kappa = determine_kappa(data, beta)
+    kappa = determine_kappa(data, beta, quantity='distance')
+    kappa_income = determine_kappa(data, np.absolute(beta), quantity='JOIE001')
     # initialize dataframe
-    results = pd.DataFrame(np.nan, index=np.arange(10), columns=['State','City', 'Kappa', 'Beta', 'Epsilon', 'Kolm-Pollak EDE', 'Atkinson EDE', 'Atkinson Adjusted EDE', 'Kolm-Pollak Index', 'Atkinson Index', 'Atkinson Adjusted Index', 'Gini Index', 'Distribution Mean', 'Distribution Max', 'Distribution Standard Deviation', 'Distribution Coefficient of Variation'])
+    results = pd.DataFrame(np.nan, index=np.arange(10), columns=['State','City', 'Kappa', 'Beta', 'Kolm-Pollak EDE', 'Atkinson EDE', 'Atkinson Adjusted EDE', 'Kolm-Pollak Index', 'Atkinson Index', 'Atkinson Adjusted Index', 'Gini Index', 'Distribution Mean', 'Distribution Max', 'Distribution Standard Deviation', 'Distribution Coefficient of Variation','Income EDE'])
     results.State = states
     results = results.set_index('State')
     # evaluate equality
@@ -50,22 +50,28 @@ def main():
         # adds the city name
         results.City = city
         # adds aversion params
-        results.loc[state, 'Kappa'], results.loc[state, 'Beta'], results.loc[state, 'Epsilon'] = kappa, beta, epsilon
+        results.loc[state, 'Kappa'], results.loc[state, 'Beta'] = kappa, beta
         # adds all Kolm-Pollak metrics
         results.loc[state, 'Kolm-Pollak Index'], results.loc[state, 'Kolm-Pollak EDE'] = inequality_function.kolm_pollak_index(a, beta, kappa, weight), inequality_function.kolm_pollak_ede(a, beta, kappa, weight)
         # adds all normal (with inverted x) atkinson metrics
-        results.loc[state, 'Atkinson Index'], results.loc[state, 'Atkinson EDE'] = inequality_function.atkinson_index(at, epsilon, weight), inequality_function.atkinson_ede(at, epsilon, weight)
+        results.loc[state, 'Atkinson Index'], results.loc[state, 'Atkinson EDE'] = inequality_function.atkinson_index(at, np.absolute(beta), weight), inequality_function.atkinson_ede(at, np.absolute(beta), weight)
         # adds all adjusted atkinson metrics
         results.loc[state, 'Atkinson Adjusted Index'], results.loc[state, 'Atkinson Adjusted EDE'] = inequality_function.atkinson_adjusted_index(a, beta, weight), inequality_function.atkinson_adjusted_ede(a, beta, weight)
         # adds gini
         results.loc[state, 'Gini Index'] = inequality_function.gini_index(a, beta, weight)
         # adds all summary stats from the distribution
         results.loc[state, 'Distribution Mean'], results.loc[state, 'Distribution Max'], results.loc[state, 'Distribution Standard Deviation'], results.loc[state, 'Distribution Coefficient of Variation'] = get_stats(df)
+        # adds KP for income
+        weight = [weight[i] for i in range(len(df)) if not np.isnan(df.loc[i,'JOIE001'])]
+        incomes = [df.loc[i,'JOIE001'] for i in range(len(df)) if not np.isnan(df.loc[i,'JOIE001'])]
+        results.loc[state, 'Income EDE'] = inequality_function.kolm_pollak_ede(incomes, kappa=kappa_income, weight=weight)
 
     #plot_gini(data)
     #plot_hist(data)
-    #plot_cdf(data)
-    results.to_csv('/homedirs/man112/access_inequality_index/data/results/food_des/{}_{}.csv'.format(file_name,weight_code))
+    plot_scatter(results)
+    # plot_cdf(data)
+    results.to_csv('/homedirs/man112/access_inequality_index/data/results/{}_{}.csv'.format(file_name,weight_code))
+
 
 def get_data():
     '''fills a dictionary of dataframes for each state'''
@@ -85,17 +91,18 @@ def get_data():
         data['{}_data'.format(state)] = df # replaces the dataframe in the dictionary
     return(city, data)
 
-def determine_kappa(data, beta):
+def determine_kappa(data, beta, quantity):
     '''takes dictionary of dataframes and beta, minimised the sum of squares for all data to return kappa'''
     # prepare the data
     kappa_data = [] # init empty list for each distance
     for state in states:
         df = data['{}_data'.format(state)]
         count = 0
-        for i in df['distance']: #takes each distance
-            for pop in range(int(df['H7X001'].iloc[count])): #adds the distance to the list as many times as there are people in the block
-                kappa_data.append(i)
-            count += 1
+        for i in df[quantity]: #takes each distance
+            if not np.isnan(i):
+                for pop in range(int(df['H7X001'].iloc[count])): #adds the distance to the list as many times as there are people in the block
+                    kappa_data.append(i)
+                count += 1
     # calculate the kappa
     kappa = inequality_function.calc_kappa(kappa_data, beta)
     return(kappa)
@@ -115,6 +122,100 @@ def get_stats(df):
     std = np.std(hist_data)
     cov = std/mean
     return(mean, max, std, cov)
+
+###
+# Plots
+###
+def plot_cdf(data = None):
+    '''plots a cdf from a dataframe'''
+    if not data:
+        city, data = get_data()
+    for state in states:#['tx','il']:
+        for group in ['H7X001']:#,'H7X002','H7X003','H7Y003']:
+            df = data['{}_data'.format(state)].copy() #gets correct dataframe
+            pop_tot = df[group].sum()
+            df = df.sort_values(by='distance')
+            df['pop_perc'] = df[group].cumsum()/pop_tot*100 #percentage of pop
+            plt.plot(df.distance, df.pop_perc, label=state+'_'+group) #plot the cdf
+    # labels
+    plt.ylabel('% Residents')
+    plt.xlabel('Distance to the nearest supermakrt (km)'.format())
+    plt.legend(loc='best')
+    # limits
+    plt.xlim([0,10])
+    plt.ylim([0,None])
+    # savefig
+    fig_out = '/homedirs/man112/access_inequality_index/fig/CDF_{}.pdf'.format(group)
+    plt.savefig(fig_out, dpi=500, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
+
+def plot_scatter(results):
+    '''
+    scatter plot the equality metrics
+    '''
+    ## Mean vs Gini
+    x = 'Distribution Mean'; y = 'Gini Index'; lbl = 'City'
+    ax = results.plot.scatter(x=x, y=y, c='#1E386A')
+    results[[x,y,lbl]].apply(lambda row: ax.text(*row),axis=1);
+    # labels
+    plt.ylabel(y)
+    plt.xlabel('Average distance to the nearest store (km)')
+    # limits
+    plt.xlim([0,3.5])
+    plt.ylim([0,0.6])
+    # savefig
+    fig_out = '/homedirs/man112/access_inequality_index/fig/scatter_gini.pdf'
+    plt.savefig(fig_out, dpi=500, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
+
+    ## EDE: distance vs income
+    x = 'Income EDE'; y = 'Kolm-Pollak EDE'; lbl = 'City'
+    ax = results.plot.scatter(x=x, y=y, c='#1E386A')
+    results[[x,y,lbl]].apply(lambda row: ax.text(*row),axis=1);
+    # labels
+    plt.ylabel('EDE of distance to the nearest store (km)')
+    plt.xlabel('EDE of median household income ($)')
+    # limits
+    plt.xlim([0,70000])
+    plt.ylim([0,4])
+    # savefig
+    fig_out = '/homedirs/man112/access_inequality_index/fig/scatter_income.pdf'
+    plt.savefig(fig_out, dpi=500, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
+
+
+
+def plot_scatter_ede(results):
+    '''
+    scatter plot the equality metrics
+    '''
+    ## Mean vs Gini
+    x = 'Distribution Mean'; y = 'Gini Index'; lbl = 'City'
+    ax = results.plot.scatter(x=x, y=y, c='#1E386A')
+    results[[x,y,lbl]].apply(lambda row: ax.text(*row),axis=1);
+    # labels
+    plt.ylabel(y)
+    plt.xlabel('Average distance to the nearest store (km)')
+    # limits
+    plt.xlim([0,3.5])
+    plt.ylim([0,0.6])
+    # savefig
+    fig_out = '/homedirs/man112/access_inequality_index/fig/scatter_gini.pdf'
+    plt.savefig(fig_out, dpi=500, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
+
+    ## EDE: distance vs income
+    x = 'Income EDE'; y = 'Kolm-Pollak EDE'; lbl = 'City'
+    ax = results.plot.scatter(x=x, y=y, c='#1E386A')
+    results[[x,y,lbl]].apply(lambda row: ax.text(*row),axis=1);
+    # labels
+    plt.ylabel('EDE of distance to the nearest store (km)')
+    plt.xlabel('EDE of median household income ($)')
+    # limits
+    plt.xlim([0,None])
+    plt.ylim([0,3.5])
+    # savefig
+    fig_out = '/homedirs/man112/access_inequality_index/fig/scatter_income.pdf'
+    plt.savefig(fig_out, dpi=500, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
+
+
+
 
 def plot_gini(data):
     '''Takes dictionary of dataframes and plots gini on one plot'''
@@ -166,10 +267,9 @@ def plot_hist(data):
     plt.savefig(fig_out, format='pdf')#, bbox_inches='tight')
     plt.clf()
 
-def plot_cdf(data=None):
+def calc_percentiles(data=None):
     '''takes dictionary of dataframes with pop and dist. returns cdf'''
     if data is None:
-        # file_name = 'food_des_{}'.format(beta)
         city, data = get_data()
     percentiles = pd.DataFrame()
     for state in states:
@@ -178,72 +278,20 @@ def plot_cdf(data=None):
         df = df.sort_values(by='distance')
         df['pop_perc'] = df.H7X001.cumsum()/pop_tot*100 #percentage of pop
         df['state'] = state
-        plt.plot(df.distance, df.pop_perc, label = state) #plot the cdf
         percentiles = percentiles.append(df[['state','distance','pop_perc']], ignore_index=True)
-    # plt.plot(np.arange(0,100,100/10000), 10000*[100], '--') #plot y=100% line
-    # labels
-    plt.ylabel('% Residents')
-    plt.xlabel('Distance to the nearest supermarket (km)'.format())
-    plt.title('CDF: Distance to the nearest supermarket'.format(), loc='center')
-    plt.legend(loc='best')
-    # limits
-    plt.xlim([0,15])
-    plt.ylim([0,None])
-    # savefig
-    fig_out = '/homedirs/man112/access_inequality_index/data/results/food_des/CDF.pdf'.format()
-    if os.path.isfile(fig_out):
-        os.remove(fig_out)
-    plt.savefig(fig_out, dpi=500, format='pdf', transparent=False)#, bbox_inches='tight')
-    # plt.clf()
-    # save the percentiles as a dataframe as well
+    # select which percentiles
     percentiles['keep'] = False
     for p in [10,50,75,90,95,100]:
         percentiles['dif'] = (percentiles.pop_perc-p).abs()
         percentiles.loc[percentiles.groupby('state')['dif'].idxmin(),'keep'] = True
-    print(percentiles)
     percentiles = percentiles.loc[percentiles.keep,]
     percentiles['pop_perc'] = percentiles['pop_perc'].round(0).apply(str)
     percentiles['distance'] = percentiles['distance'].round(2)
     percentiles = percentiles.drop_duplicates(["state", "pop_perc"])
     percentiles = percentiles.pivot(index='state', columns='pop_perc', values='distance')
-    print(percentiles)
+    # save
     percentiles.to_csv('/homedirs/man112/access_inequality_index/data/results/food_des/distance_percentiles.csv')
 
-
-def plot_cdf_dems(data = None):
-    '''plots a cdf from a data frame'''
-    if not data:
-        # file_name = 'food_des_{}'.format(beta)
-        city, data = get_data()
-
-    for state in ['tx','il']:
-        for race in ['H7X001','H7X002','H7X003','H7Y003']:
-            df = data['{}_data'.format(state)].copy() #gets correct dataframe
-            pop_tot = df[race].sum()
-            df = df.sort_values(by='distance')
-            df['pop_perc'] = df[race].cumsum()/pop_tot*100 #percentage of pop
-            plt.plot(df.distance, df.pop_perc, label = state+'_'+race) #plot the cdf
-    # plt.plot(np.arange(0,100,100/10000), 10000*[100], '--') #plot y=100% line
-    # labels
-    plt.ylabel('% Residents')
-    plt.xlabel('Distance to the nearest supermakrt (km)'.format())
-    # plt.title('CDF: Distance to the nearest supermarket'.format(), loc='center')
-    plt.legend(loc='best')
-    # limits
-    plt.xlim([0,10])
-    plt.ylim([0,None])
-    # horizontal line at 0
-    plt.axhline(y = 0, color = 'black', linewidth = 1.3, alpha = .7)
-    # vertical line on left
-    # plt.set_xlim(left = 0, right = 10)
-    # background
-    # plt.set_facecolor('w')
-    # savefig
-    fig_out = '/homedirs/man112/access_inequality_index/fig/CDF_fooddesert_bestworst.pdf'.format()
-    if os.path.isfile(fig_out):
-        os.remove(fig_out)
-    plt.savefig(fig_out, dpi=500, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
-    # plt.clf()
 
 def plot_edes(data = None):
     '''plots the ede and inequality indices'''
@@ -332,62 +380,6 @@ def plot_edes_dem():
     fig_out = '/homedirs/man112/access_inequality_index/fig/index_race_compare.pdf'.format()
     # plt.show()
     plt.savefig(fig_out, dpi=500, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
-
-
-def sensitivity_aversion():
-    ''' plot how the EDE and IE changes with beta '''
-    # get the data
-    city, data = get_data() # data is a dictionary of dataframes for each state
-    kappa_data = get_kappa_data(data)
-    # initiate list
-    results = list()
-    # loop through beta values
-    for beta in np.concatenate(([-0.01,-0.25,-0.5,-0.75, -1.5, -2],np.logspace(0,1)*-1),axis=None):#[0,-0.25,-0.5,-0.75,-1,-2,-5,-10,-100]:
-        # calculate kappa from beta
-        kappa = inequality_function.calc_kappa(kappa_data, beta) # kappa is based on the distances from ALL states and the beta provided
-        # calculate the ede for each city
-        for state in states:#['il','tx','fl']:
-            # get the data subset
-            df = data['{}_data'.format(state)].copy()
-            a = list(df.distance)
-            weight = list(df['H7X001'])
-            # calculate the values
-            ede, ie = inequality_function.kolm_pollak_ede(a, kappa = kappa, weight = weight), inequality_function.kolm_pollak_index(a, kappa = kappa, weight = weight)
-            # add to list
-            new_result = [cities[state], beta, ede, ie]
-            results.append(new_result)
-    # make list of lists a dataframe
-    results = pd.DataFrame(results, columns = ['city','beta','ede','ie'])
-    results_ede = results.pivot(index='beta', columns='city', values='ede')
-    results_ie = results.pivot(index='beta', columns='city', values='ie')
-
-    # print(results)
-
-    # plot the results
-    ax = plt.axes()
-    plt.locator_params(axis='y', nbins=4)
-    results_ede.plot(ax=ax)
-    plt.ylim([0, 20])
-    plt.gca().invert_xaxis()
-    fig_out = '/homedirs/man112/access_inequality_index/fig/sensitivity_aversion.pdf'
-    plt.savefig(fig_out, dpi=800, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
-    plt.clf()
-
-    # plot a selection of betas
-    results_beta = results.copy()
-    results_beta['beta'] = results_beta['beta'].round(2).apply(str)
-    results_beta = results_beta.pivot(index='city', columns='beta', values='ede')
-    results_beta = results_beta.sort_values(by='-0.5')
-    print(results_beta)
-    ax = plt.axes()
-    plt.locator_params(axis='y', nbins=3)
-    results_beta.plot(y=['-0.25','-0.5','-0.75','-1.0','-1.5','-2.0'],ax=ax)
-    plt.xticks(range(10),results_beta.index)
-    plt.xticks(rotation=90)
-    plt.ylim([0, 9])
-    plt.yticks([0,3,6,9])
-    fig_out = '/homedirs/man112/access_inequality_index/fig/sensitivity_aversion_cities.pdf'
-    plt.savefig(fig_out, dpi=800, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
 
 
 def city_dems():
---
+++ src/evaluate_sensitivity.py
@@ -0,0 +1,194 @@
+'''
+For given beta and states, will take data from SQL with population and distance and provide:
+    Kolm-Pollak Index & EDE
+    Atkinson Index & EDE
+    Adjusted Atkinson Index and EDE
+    Gini Index
+    Plot of Gini, CDF and Distribution
+    Distribution summary statistics
+'''
+
+# User defined variables
+states = ['md','fl', 'co', 'mi', 'la', 'ga', 'or', 'il', 'wa', 'tx']
+cities = {'md':'baltimore','fl':'miami','co':'denver','mi':'detroit','la':'new orleans','ga':'atlanta','or':'portland','il':'chicago','wa':'seattle','tx':'houston'}
+weight_code = 'H7X001'
+
+# Imports
+import utils
+from config import *
+import inequality_function
+import matplotlib
+from tqdm import tqdm
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+import matplotlib.style as style
+style.use('fivethirtyeight')
+w = 5
+h = w/1.618
+
+def main():
+    '''Changes aversion parameter and calculates result'''
+    city, data = get_data()
+    results_fn = '/homedirs/man112/access_inequality_index/data/results/EDEs_{}.csv'.format(weight_code)
+    if os.path.isfile(results_fn):
+        results = pd.read_csv(results_fn)
+    else:
+        # initiate list
+        results = list()
+        # loop through beta values
+        for beta in tqdm(np.concatenate(([0,-0.25,-0.5,-0.75, -1.5, -2],np.logspace(0,1)*-1),axis=None)):
+            # calculate kappa from beta -- kappa is based on the distances from ALL states and the beta provided
+            kappa = determine_kappa(data, beta, quantity='distance')
+            # calculate the ede for each city
+            for state in states:
+                # get the data subset
+                df = data['{}_data'.format(state)].copy()
+                # calculate the values
+                ede = inequality_function.kolm_pollak_ede(list(df.distance), kappa = kappa, weight = list(df['H7X001']))
+                # add to list
+                new_result = [cities[state], beta, ede]
+                results.append(new_result)
+        # make list of lists a dataframe
+        results = pd.DataFrame(results, columns = ['city','beta','ede'])
+        # save result
+        results.to_csv(results_fn)
+    # plots
+    # plot_aversion_continuous(results)
+    # plot_aversion_discrete(results)
+    plot_mean_ede_bycity(results, data)
+
+
+def get_data():
+    '''fills a dictionary of dataframes for each state'''
+    data = {} # init empty dictionary
+    city = [] #init empty list of cities to return for the results
+    for state in states:
+        db, context = cfg_init(state)
+        cursor = db['con'].cursor()
+        sql = 'SELECT * FROM distxdem'
+        data["{}_data".format(state)] = pd.read_sql(sql, db["con"])
+        db['con'].close()
+        city.append(context['city'])
+        df = data['{}_data'.format(state)]
+        df = df.loc[df['distance'] !=0] # removes all rows with 0 distance
+        df = df.loc[df['H7X001'] !=0] # removes all rows with 0 population
+        df.distance = df.distance/1000 # converts from meters to Kms
+        data['{}_data'.format(state)] = df # replaces the dataframe in the dictionary
+    return(city, data)
+
+
+def determine_kappa(data, beta, quantity):
+    '''takes dictionary of dataframes and beta, minimised the sum of squares for all data to return kappa'''
+    # prepare the data
+    kappa_data = [] # init empty list for each distance
+    for state in states:
+        df = data['{}_data'.format(state)]
+        count = 0
+        for i in df[quantity]: #takes each distance
+            if not np.isnan(i):
+                for pop in range(int(df['H7X001'].iloc[count])): #adds the distance to the list as many times as there are people in the block
+                    kappa_data.append(i)
+                count += 1
+    # calculate the kappa
+    kappa = inequality_function.calc_kappa(kappa_data, beta)
+    return(kappa)
+
+###
+# Plots
+###
+def plot_aversion_continuous(results):
+    # pivot the dataset
+    results_ede = results.pivot(index='beta', columns='city', values='ede')
+    # plot the results
+    ax = plt.axes()
+    plt.locator_params(axis='y', nbins=4)
+    results_ede.plot(ax=ax)
+    plt.ylim([0, 20])
+    plt.gca().invert_xaxis()
+    fig_out = '/homedirs/man112/access_inequality_index/fig/sensitivity_aversion.pdf'
+    plt.savefig(fig_out, dpi=800, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
+    plt.clf()
+
+def plot_aversion_discrete(results):
+    # plot a selection of betas
+    results_beta = results.copy()
+    results_beta['beta'] = results_beta['beta'].round(2).apply(str)
+    results_beta = results_beta.pivot(index='city', columns='beta', values='ede')
+    results_beta = results_beta.sort_values(by='-0.5')
+    ax = plt.axes()
+    plt.locator_params(axis='y', nbins=3)
+    results_beta.plot(y=['-0.25','-0.5','-0.75','-1.0','-1.5','-2.0'],ax=ax)
+    plt.xticks(range(10),results_beta.index)
+    plt.xticks(rotation=90)
+    plt.ylim([0, 9])
+    plt.yticks([0,3,6,9])
+    fig_out = '/homedirs/man112/access_inequality_index/fig/sensitivity_aversion_cities.pdf'
+    plt.savefig(fig_out, dpi=800, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
+
+def plot_mean_ede(results, data):
+    # calculate the mean for each city
+    results_mean = []
+    for state in states:
+        # get the data subset
+        df = data['{}_data'.format(state)].copy()
+        # calculate the values
+        value = np.average(list(df.distance), weights = list(df['H7X001']))
+        # add to list
+        new_result = [cities[state], 'mean', value]
+        results_mean.append(new_result)
+    results_mean = pd.DataFrame(results_mean, columns = ['city','beta','mean'])
+    results_mean = results_mean.pivot(index='city', columns='beta', values='mean')
+    # add mean to EDE table
+    results = pd.merge(results,results_mean, on='city')
+    # format for plot
+    results_beta = results.copy()
+    results_beta['beta'] = results_beta['beta'].round(2).apply(str)
+    print(results_beta)
+    results_beta = results_beta.pivot(index='mean', columns='beta', values='ede')
+    # results_beta = results_beta.sort_values(by='-0.5')
+    ax = plt.axes()
+    # plt.locator_params(axis='y', nbins=5)
+    results_beta.plot(y=['-0.25','-0.5','-0.75','-1.0','-1.5','-2.0'],ax=ax,style='o-')
+    plt.ylim([0, None])
+    plt.xlabel('Average distance to nearest store')
+    plt.ylabel('EDE distance to nearest store')
+    fig_out = '/homedirs/man112/access_inequality_index/fig/mean_ede.pdf'
+    plt.savefig(fig_out, dpi=800, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
+
+def plot_mean_ede_bycity(results, data):
+    # calculate the mean for each city
+    results_mean = []
+    for state in states:
+        # get the data subset
+        df = data['{}_data'.format(state)].copy()
+        # calculate the values
+        value = np.average(list(df.distance), weights = list(df['H7X001']))
+        # add to list
+        new_result = [cities[state], 'mean', value]
+        results_mean.append(new_result)
+    results_mean = pd.DataFrame(results_mean, columns = ['city','beta','mean'])
+    results_mean = results_mean.pivot(index='city', columns='beta', values='mean')
+    # add mean to EDE table
+    results = pd.merge(results,results_mean, on='city')
+    # format for plot
+    results_beta = results.copy()
+    results_beta['beta'] = results_beta['beta'].round(2).apply(str)
+    results_beta = results_beta[results_beta.beta.isin(['-0.25','-0.5','-0.75','-1.0','-1.5','-2.0'])]
+    ax = plt.axes()
+    results_beta.groupby('city').plot(x='mean',y='ede',ax=ax,style='o-')
+    # results_beta = results_beta.pivot(index='mean', columns='city', values='ede')
+    # results_beta = results_beta.sort_values(by='-0.5')
+
+    # plt.locator_params(axis='y', nbins=5)
+    # results_beta.plot(y=['-0.25','-0.5','-0.75','-1.0','-1.5','-2.0'],ax=ax,style='o-')
+    plt.ylim([0, None])
+    plt.xlabel('Average distance to nearest store')
+    plt.ylabel('EDE distance to nearest store')
+    xy_line = (np.min(results_beta.ede),np.max(results_beta.ede))
+    ax.plot(xy_line,xy_line, 'k--')
+    fig_out = '/homedirs/man112/access_inequality_index/fig/mean_ede.pdf'
+    plt.savefig(fig_out, dpi=800, format='pdf', transparent=True, bbox_inches='tight',facecolor='w')
+
+
+if __name__ == '__main__':
+    main()
